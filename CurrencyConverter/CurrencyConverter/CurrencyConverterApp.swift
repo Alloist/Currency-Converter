6a62dff02ef4ef38842a3c85a979f64a823839d0
Aliaksei Gorodji <a.gorodji@gmail.com>
2025-06-11 18:29:59 +0300

    create project

---
+++ CurrencyConverter/CurrencyConverter/CurrencyConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  CurrencyConverterApp.swift
+//  CurrencyConverter
+//
+//  Created by Aliaksei Gorodji on 11.06.25.
+//
+
+import SwiftUI
+
+@main
+struct CurrencyConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
